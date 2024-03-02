c5f4fedfb2ae69dd2927045b842172788a631877
emmalynnnn <emmaruthlynn@gmail.com>
2024-03-02 15:09:23 -0700

    MUDKIP

---
+++ healthyhoroscopes/src/components/views/Mudkip.js
@@ -0,0 +1,12 @@
+import mudkip from '../../assets/mudkip.png'
+
+const Mudkip = () => {
+
+  return (<>
+
+    <img style={{height: '900px'}} src={mudkip} alt="react logo" />
+
+  </>);
+};
+
+export default Mudkip;
