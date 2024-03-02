1f9dd707c164fd5876c2af25439f8733e6039f59
emmalynnnn <emmaruthlynn@gmail.com>
2024-03-01 22:32:02 -0700

    adding router and setting up base pages

--- healthyhoroscopes/src/App.js
+++ healthyhoroscopes/src/App.js
@@ -5,12 +5,39 @@ import {
 } from "@supabase/auth-ui-shared";
 import { createClient } from "@supabase/supabase-js";
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/views/Home';
+import Affirmations from "./components/views/Affirmations";
+import Journaling from "./components/views/Journaling";
+import Music from "./components/views/Music";
+import Activity from "./components/views/Activity";
+import Meditations from "./components/views/Meditations";
+import Resources from "./components/views/Resources";
+
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
-const App = () => (
-  <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+
+        <Route exact path="/login" element={<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />} />
+
+        <Route path="/activities/affirmations" element={<Affirmations/>} />
+        <Route path="/activities/journaling" element={<Journaling/>} />
+        <Route path="/activities/music" element={<Music/>} />
+        <Route path="/activities/activity" element={<Activity/>} />
+        <Route path="/activities/meditations" element={<Meditations/>} />
+        <Route path="/activities/resources" element={<Resources/>} />
+
+      </Routes>
+    </Router>
+  );
+};
+
+
 export default App;
---
+++ healthyhoroscopes/src/components/views/Meditations.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Meditations = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Meditations: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Meditations;
