e3d12ec93fb41cd2f25a4e75e6c65757cbadf379
Treyson Grange <treyson.grange@outlook.com>
2024-03-02 00:41:22 -0700

    :pencil2: treysons site and db stuff

--- healthyhoroscopes/src/App.js
+++ healthyhoroscopes/src/App.js
@@ -5,14 +5,15 @@ import {
 } from "@supabase/auth-ui-shared";
 import { createClient } from "@supabase/supabase-js";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/views/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/views/Home";
 import Affirmations from "./components/views/Affirmations";
 import Journaling from "./components/views/Journaling";
 import Music from "./components/views/Music";
 import Activity from "./components/views/Activity";
 import Meditations from "./components/views/Meditations";
 import Resources from "./components/views/Resources";
+import Treyson from "./components/views/Treyson";
 
 import LightBackground from "./components/elements/LightBackground";
 
@@ -25,23 +26,30 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={
-          <LightBackground content={<Home/>}/>
-        } />
+        <Route
+          exact
+          path="/"
+          element={<LightBackground content={<Home />} />}
+        />
 
-        <Route exact path="/login" element={<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />} />
+        <Route
+          exact
+          path="/login"
+          element={
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+          }
+        />
 
-        <Route path="/activities/affirmations" element={<Affirmations/>} />
-        <Route path="/activities/journaling" element={<Journaling/>} />
-        <Route path="/activities/music" element={<Music/>} />
-        <Route path="/activities/activity" element={<Activity/>} />
-        <Route path="/activities/meditations" element={<Meditations/>} />
-        <Route path="/activities/resources" element={<Resources/>} />
-
+        <Route path="/activities/affirmations" element={<Affirmations />} />
+        <Route path="/activities/journaling" element={<Journaling />} />
+        <Route path="/activities/music" element={<Music />} />
+        <Route path="/activities/activity" element={<Activity />} />
+        <Route path="/activities/meditations" element={<Meditations />} />
+        <Route path="/activities/resources" element={<Resources />} />
+        <Route path="/treyson" element={<Treyson />} />
       </Routes>
     </Router>
   );
 };
-
 
 export default App;
