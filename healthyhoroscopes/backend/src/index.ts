aa9ed00d10673e07be31aae9f52aad10974821ed
Bethany Christensen <a02299515@aggies.usu.edu>
2024-03-02 10:18:00 -0700

    music recommendation api set up

--- healthyhoroscopes/backend/src/index.ts
+++ healthyhoroscopes/backend/src/index.ts
@@ -3,7 +3,7 @@ import bodyParser = require('body-parser');
 const cors = require('cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
-import {PythonShell} from 'python-shell';
+import {Options, PythonShell} from 'python-shell';
 import { config } from 'dotenv'
 
 config({ path: '../.env' })
@@ -16,6 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 2999;
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const modelPath = "../models/trained_sentiment_analysis_model.joblib";
+
+interface MusicRecommend {
+  Anxiety : number,
+  Depression : number,
+  OCD: number,
+  Insomnia: number,
+  FavGenre : string,
+  WhileWorking: number,
+  Instrumentalist: number,
+  Exploratory: number,
+  ForeignLanguages:number,
+  HoursPerDay: number,
+  Age: number,
+  Composer: number  
+}
 
 
 app.use(cors({
@@ -89,6 +104,7 @@ async function horoscope(bday:string, color:string) {
 
 }
 
+//affirmation
 app.post("/model/affirmation", async (req:Request, res: Response)=> {
   const affirmation = req.body.affirmation;
   PythonShell.runString(`import joblib
@@ -107,6 +123,22 @@ print(prediction[0])`
   
 });
 
+//music
+app.post("/model/music", async (req:Request, res:Response) => {
+  const mr = req.body as MusicRecommend
+  let options: Options = {
+    mode: 'text',
+    scriptPath: '../models/',
+    args: [`Age:${mr.Age},HoursPerDay:${mr.HoursPerDay},WhileWorking:${mr.WhileWorking},Instrumentalist:${mr.Instrumentalist},Composer:${mr.Composer},Exploratory:${mr.Exploratory},ForeignLanguages:${mr.ForeignLanguages},Anxiety:${mr.Anxiety},Depression:${mr.Depression},Insomnia:${mr.Insomnia},OCD:${mr.OCD},FavGenre:${mr.FavGenre}`]
+  };
+  
+  PythonShell.run('music_script.py', options).then(genres=>{    
+    console.log(genres)
+    res.json({genres})
+  });
+
+  
+});
 
 app.listen(port, () => {
   console.log(`tHe SeRvEr Is RuNnInG oN pOrT: ${port}`);
